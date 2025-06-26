c0d7fe7e9698f01f32ef39142eeddf594a7f5f9b
Suraj7149 <santoshsuraj7149@gmail.com>
2025-06-26 17:10:27 +0530

    Switching to windows 11.

---
+++ React and Node.js/todo-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-grey-600 text-white'>This is a To-Do App.</h1>
+    </div>
+  )
+}
+
+export default App
